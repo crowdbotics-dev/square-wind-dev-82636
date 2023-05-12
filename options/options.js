fc4158e38a4c025183edd096f2186e28f7f33d03
Crowdbotics <engineering@crowdbotics.com>
2023-05-12 08:24:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "square-wind-dev-82636",
+  url: "https://square-wind-dev-82636.botics.co",
+  api: "https://square-wind-dev-82636.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
